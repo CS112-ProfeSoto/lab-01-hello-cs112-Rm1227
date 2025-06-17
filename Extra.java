b941b1e973e6191ea85f403075526524077c848a
Ruben Major Jr <117961830+Rm1227@users.noreply.github.com>
2025-06-17 02:31:01 +0000

    Finished

---
+++ Extra.java
@@ -0,0 +1,29 @@
+//function of Extra.java is to define names and set up for calling in main, also has a block that checks for name equivelancy
+public class Extra {
+    private String name;  //single instance variable marked here
+    public Extra() {  //constructor with no arguments
+        this.name = "";
+    }
+    public Extra(String name) { //constructor that builds and intitializes "name"
+        this.name = name;
+    }
+     public String toString() { //usage of tostring method
+        return "Extra{name='" + name + "'}";
+    }
+    public void setName(String name) {  // mutator allowing name changes after construction
+        this.name = name;
+    }
+    public String getName() { // accessor, allows the name value ot be read (accessed)
+        return this.name;
+    }
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Extra)) return false; // equals() override and block that logically checks for whether or not the values in the name field are quivelant or not and prints that when called on in the main class
+        Extra other = (Extra) obj;
+        if (name == null) {
+            return other.name == null;
+        }
+        return name.equals(other.name);
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!"); //Default warmup message, Yay!
+        shout();            
+        int sum = add(20, 189);             
+        System.out.println("20 + 189 = " + sum); //simple addition printout
+        Extra e1 = new Extra("CoolName"); 
+            System.out.println(e1);
+            Extra e2 = new Extra();
+            e2.setName("CoolerName");
+            System.out.println(e2.getName()); //extra class demonstration
+
+            System.out.println(e1.equals(e2)); //compares and prints whether true or false
+    }
+    // void static method
+    public static void shout() {
+        System.out.println("Time to learn Java!");
+    }
+    // static method that returns some values
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
